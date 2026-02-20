8ba4ce40490fd57bc93be3da43c7303fee0137bc
GlenCarvajalHidalgo <ggdegleng@gmail.com>
2026-02-20 16:26:10 -0600

    Second commit

---
+++ ClimatesRedRiver/Index.js
@@ -0,0 +1,20 @@
+
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                'rose-gold': '#b76e79',
+                'rose-gold-light': '#d4a5ab',
+                'mountain-green': '#2d4a3e',
+                'forest': '#1a2e26',
+                'cream': '#faf9f7',
+                'warm-white': '#fefefe'
+            },
+            fontFamily: {
+                'serif': ['Cormorant Garamond', 'serif'],
+                'sans': ['Montserrat', 'sans-serif'],
+            }
+        }
+    }
+}
+
